37e6ba0ec958efd528ca0b604a3bf37b4065c515
Daisy <66263341+bloomsirenix@users.noreply.github.com>
2024-02-07 23:36:00 +0100

    fixed AI fuckup
    its iptables you darn bafoon AI

--- tinyblackhole.sh
+++ tinyblackhole.sh
@@ -3,10 +3,10 @@
 # Define IP ranges to block
 IP_RANGES="5.136.0.0/13 95.24.0.0/13 176.208.0.0/13 178.64.0.0/13 10.0.0.0/12 172.16.0.0/12 192.168.0.0/16 45.93.20.0/24 62.122.184.0/24 62.233.50.0/24 85.209.11.0/24 87.247.158.0/23 91.213.138.0/24 91.240.118.0/24 91.241.19.0/24 152.89.196.0/24 152.89.198.0/24 176.111.174.0/24 185.11.61.0/24 185.81.68.0/24 185.122.204.0/24 185.198.69.0/24 185.234.216.0/24 188.119.66.0/24 194.26.135.0/24 195.226.194.0/24"
 
-# Block IP ranges using nftables
+# Block IP ranges using iptables
 for IP_RANGE in $IP_RANGES; do
-    nft add rule ip filter input ip saddr $IP_RANGE drop
-    nft add rule ip filter output ip daddr $IP_RANGE drop
+    iptables -A INPUT -s $IP_RANGE -j DROP
+    iptables -A OUTPUT -d $IP_RANGE -j DROP
 done
 
-echo "Firewalls configured to block specified IP ranges using nftables."
+echo "Firewalls configured to block specified IP ranges using iptables."
